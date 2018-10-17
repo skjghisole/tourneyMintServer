34ab559c577132832f3b10bb061ef944f5e60c62
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2018-10-18 00:51:31 +0800

    fixed port bugs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
-const app = require('./app');
+const server = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
@@ -8,6 +8,15 @@ process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+function startServer = () => {
+	const app = await server();
+	try {
+    app.listen(process.env.PORT || app.get('port'), () => {
+      console.log(`App Live! http://localhost:${process.env.PORT || app.get('port')}/`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+startServer();
