01a7dd7877337d1a36119ed0467b839b945d9d34
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2018-10-18 01:24:32 +0800

    working(?)

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
-const server = require('./app');
+const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const server = app.listen(process.env.PORT);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-function startServer = () => {
-	const app = await server();
-	try {
-    app.listen(process.env.PORT || app.get('port'), () => {
-      console.log(`App Live! http://localhost:${process.env.PORT || app.get('port')}/`);
-    });
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-startServer();
+server.on('listening', () =>
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
